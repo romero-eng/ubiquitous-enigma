72abe739254976798de5ea43c2343ef1df65c864
romero-eng <39012010+romero-eng@users.noreply.github.com>
2025-06-09 23:43:55 -0400

    Put a hack in to address a bug

--- equations.py
+++ equations.py
@@ -3,7 +3,6 @@ import numpy.typing as npt
 from typing import Callable
 
 # Calculation Modules
-import sys
 import numpy as np
 import scipy.signal as dsp
 from numpy.polynomial.chebyshev import chebval, cheb2poly
@@ -18,6 +17,8 @@ import matplotlib.pyplot as plt  # noqa: E402
 
 def plot_mag_func(b: list[float],
                   mag_func: Callable[[npt.NDArray[np.float64]], npt.NDArray[np.float64]]) -> None:
+
+    print(f"\nActual Spectral Coefficients: {str(np.array(b)):s}\n")
 
     [w, h] = dsp.freqz(b)
 
@@ -45,16 +46,17 @@ def plot_mag_func(b: list[float],
 def plot_irreducible_quadratic_mag_func(gamma_abs: float,
                                         gamma_angle_deg: float,
                                         gain: float) -> None: 
- 
+
     gamma_abs_sq = np.square(gamma_abs)
     gamma_angle = (np.pi/180)*gamma_angle_deg
     gamma = gamma_abs*np.exp(1j*gamma_angle)
 
     eta = gamma_abs_sq + np.sqrt(np.square(gamma_abs_sq) - 2*np.cos(2*gamma_angle)*gamma_abs_sq + 1)
     rho_abs = np.sqrt(eta - np.sqrt(np.square(eta) - 1))
-    rho_angle = (1/2)*np.arccos(2*gamma_abs_sq - eta) 
-
+    rho_angle = np.arctan(np.tan(gamma_angle)/np.tanh(np.log(rho_abs)))
     A = gain/(2*rho_abs)
+
+    rho_angle += np.pi*np.heaviside(gamma_angle_deg - 90, 0)
 
     plot_mag_func([A, 2*A*rho_abs*np.cos(rho_angle), A*np.square(rho_abs)],
                   lambda w: gain*np.sqrt(np.abs((np.cos(w) + gamma)*(np.cos(w) + np.conjugate(gamma)))))
@@ -62,5 +64,5 @@ def plot_irreducible_quadratic_mag_func(gamma_abs: float,
 
 if (__name__ == "__main__"):
 
-    plot_irreducible_quadratic_mag_func(2, float(sys.argv[1]), 1/2.9)
+   plot_irreducible_quadratic_mag_func(2, 45, 1/2.8)
 
