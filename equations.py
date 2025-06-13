a2361ec7f121fa37c1b9cc95aa8053a2d0176f1b
romero-eng <39012010+romero-eng@users.noreply.github.com>
2025-06-13 13:03:32 -0400

    Streamlined spectral magnitude calculation

--- equations.py
+++ equations.py
@@ -5,7 +5,7 @@ from typing import Callable
 # Calculation Modules
 import numpy as np
 import scipy.signal as dsp
-from numpy.polynomial.chebyshev import chebval, poly2cheb
+from numpy.polynomial.chebyshev import chebval, chebfromroots
 
 # Plotting Modules
 import os
@@ -16,12 +16,12 @@ import matplotlib.pyplot as plt  # noqa: E402
 
 
 def plot_mag_func(z_transform_coefs: npt.NDArray[np.float64],
-                  mag_sq_coefs: npt.NDArray[np.float64]) -> None:
+                  spectral_roots: npt.NDArray[np.complex128]) -> None:
 
     [omega, h_omega] = dsp.freqz(z_transform_coefs)
 
     sq_magnitudes: dict[str, npt.NDArray[np.float64]] = \
-        {"Theoretical": np.sqrt(chebval(np.cos(omega), poly2cheb(mag_sq_coefs))),
+        {"Theoretical": np.sqrt(chebval(np.cos(omega), np.pow(-1, np.sum(np.angle(spectral_roots) == 0))*np.real(chebfromroots(spectral_roots)))),
               "Actual": np.abs(h_omega)}  # noqa: E127
 
     freq = omega/(2*np.pi)
@@ -41,7 +41,7 @@ def plot_mag_func(z_transform_coefs: npt.NDArray[np.float64],
     os.remove(png_filename)
 
 
-def plot_complex_spectral_root(gamma: np.complex128) -> None: 
+def calculate_complex_spectral_root(gamma: np.complex128) -> npt.NDArray[np.float64]: 
 
     gamma_abs_sq = np.square(np.abs(gamma))
     gamma_angle = np.angle(gamma)
@@ -50,38 +50,35 @@ def plot_complex_spectral_root(gamma: np.complex128) -> None:
     rho_abs = np.sqrt(eta - np.sqrt(np.square(eta) - 1))
     rho_angle = np.arctan(np.tan(gamma_angle)/np.tanh(np.log(rho_abs))) + np.pi*np.heaviside(gamma_angle - (np.pi/2), 0)
 
-    z_coefs = (1/(2*rho_abs))*np.array([1, -2*rho_abs*np.cos(rho_angle), np.square(rho_abs)])
+    z_coefs: npt.NDArray[np.float64] = np.array([1, -2*rho_abs*np.cos(rho_angle), np.square(rho_abs)])/(2*rho_abs)
 
-    mag_sq_coefs = np.array([gamma_abs_sq, -2*np.cos(gamma_angle)*np.sqrt(gamma_abs_sq), 1])
-
-    plot_mag_func(z_coefs, mag_sq_coefs)
+    return z_coefs
 
 
-def plot_real_spectral_root(gamma: np.float64) -> None:
+def calculate_real_spectral_root(gamma: np.float64) -> npt.NDArray[np.float64]:
 
-    gamma_abs = np.abs(gamma)
-    gamma_sign = np.sign(gamma)
+    rho_abs = np.abs(gamma) - np.sqrt(np.square(gamma) - 1)
+    rho = np.sign(gamma)*rho_abs
 
-    rho_abs = gamma_abs - np.sqrt(np.square(gamma_abs) - 1)
-    rho = gamma_sign*rho_abs
+    z_coefs: npt.NDArray[np.float64] = np.array([1, -rho])/np.sqrt(2*rho_abs)
 
-    z_coefs = np.array([1, -rho])/np.sqrt(2*rho_abs)
-
-    mag_sq_coefs = -gamma_sign*np.array([-gamma, 1])
-
-    plot_mag_func(z_coefs, mag_sq_coefs)
+    return z_coefs
 
 
 if (__name__ == "__main__"):
-   
-    """ 
-    gamma_abs = 2
-    gamma_angle_deg = 45
-    gamma = gamma_abs*np.exp(1j*(np.pi/180)*gamma_angle_deg)
 
-    plot_complex_spectral_root(gamma)
-    """
-    gamma = np.array([1.33611])[0]
+    gammas = \
+        np.array([2*np.exp(1j*(np.pi/180)*45),
+                  -1.33611,
+                  3])
 
-    plot_real_spectral_root(gamma)
-    #"""
+    roots: npt.NDArray[np.complex128] = np.array([gammas[0], np.conjugate(gammas[0]), gammas[1], gammas[2]])
+
+    z_coefs_1 = calculate_complex_spectral_root(roots[0])
+    z_coefs_2 = calculate_real_spectral_root(np.float64(np.real(roots[2])))
+    z_coefs_3 = calculate_real_spectral_root(np.float64(np.real(roots[3])))
+
+    z_coefs: npt.NDArray[np.float64] = np.array([np.float64(num) for num in np.convolve(np.convolve(z_coefs_1, z_coefs_2), z_coefs_3)])
+
+    plot_mag_func(z_coefs, roots)
+
