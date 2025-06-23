439afa0bca5b73aacfa253c05a65b12aa5a5f12f
romero-eng <39012010+romero-eng@users.noreply.github.com>
2025-06-23 16:15:08 -0400

    Refined some mathematics

--- design.py
+++ design.py
@@ -45,21 +45,42 @@ def add_trough(f_e: np.float64,
                roots: list[np.complex128],
                order: int = 1) -> list[np.complex128]:
 
-    offset = np.pow(10, target_offset_dB/5) + np.square(np.cos(2*np.pi*f_e))
-    root = np.sqrt(offset)*np.exp(1j*np.arccos(np.cos(2*np.pi*f_e)/np.sqrt(offset)))
+    M = np.pow(10, target_offset_dB/10)
+    cos_omega_e = np.cos(2*np.pi*f_e)
 
-    roots += [root, np.conjugate(root)]*order
+    gamma_mag = np.sqrt(np.square(M) + np.square(cos_omega_e))
+    quant = np.arctan(M/cos_omega_e)
+    gamma_angle = quant if f_e < 0.25 else np.pi - quant
+    gamma = gamma_mag*np.exp(1j*gamma_angle)
+
+    roots += [gamma, np.conjugate(gamma)]*order
 
     return roots
 
 
 if (__name__=="__main__"):
 
+    """
     ma_roots: list[np.complex128] = []
+    ma_roots = add_trough(0.27 , 0, ma_roots, order = 4)
     ma_roots = add_trough(0.27 , 0, ma_roots, order = 4)
 
     ar_roots: list[np.complex128] = []
     ar_roots = add_trough(0.24, 0, ar_roots, order = 5)
     ar_roots = add_trough(0.05, 0, ar_roots)
 
-    plot_designed_spectrum(ma_roots, ar_roots) 
+    plot_designed_spectrum(ma_roots, ar_roots)
+    """
+
+    ar_roots = []
+    #ar_roots = add_trough(0.15, -4, ar_roots)
+    ar_roots = add_trough(0.2, -3, ar_roots, 3)
+    ar_roots = add_trough(0.25, -12, ar_roots, 2)
+
+    ma_roots = []
+    ma_roots = add_trough(0.45, -8, ma_roots, 2)
+    ma_roots = add_trough(0.4, -6, ma_roots, 2)
+    ma_roots = add_trough(0.35, -3, ma_roots, 2)
+    ma_roots = add_trough(0.3, -10, ma_roots, 2)
+
+    plot_designed_spectrum(ma_roots, ar_roots)
